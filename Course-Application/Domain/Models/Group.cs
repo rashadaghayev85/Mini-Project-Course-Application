24d84b13597a4d164785f853337ede2d2720d6c7
rashadaghayev85 <rashadra@code.edu.az>
2024-03-14 14:09:18 +0400

    Architecture prepared

---
+++ Course-Application/Domain/Models/Group.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    internal class Group:BaseEntity
+    {
+        public int Name { get; set; }
+        public int Teacher { get; set; }
+        public string Room { get; set; }
+    }
+}
